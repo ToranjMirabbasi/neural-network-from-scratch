afe7a8abdb01889009cf483e8863ba00e6e08c08
Toranj <toranjmirabbasi@gmail.com>
2026-02-24 15:14:31 +0330

    Matrix multiplication function implemented and tested

--- nnfs/matrix_utils.py
+++ nnfs/matrix_utils.py
@@ -5,14 +5,28 @@ def dot(a, b):
     if len(a) != len(b):
         raise ValueError("Dot product is undefined for vectors of different lengths")
     else:
+        print(np.multiply(a, b))
         dot_result = sum(np.multiply(a, b))
+        print(dot_result)
     return dot_result
 
 def matmul(A, B):
-    """
-    Performs matrix multiplication between A and B.
-    """
-    return A @ B
+    matrixA_dim = [len(A), len(A[0])]
+    matrixB_dim = [len(B), len(B[0])]
+    multimatrx = np.empty((matrixA_dim[0], matrixB_dim[1]))
+    print(multimatrx)
+    if matrixA_dim[1] != matrixB_dim[0]:
+        raise ValueError("Matrices cannot be multiplied")
+    else:
+        B_t = B.transpose()
+        for i in range(matrixA_dim[0]):
+            print("i =", i)
+            for j in range(matrixB_dim[1]):
+                print("j =", j)
+                element = dot(A[i], B_t[j])
+                print(element)
+                multimatrx[i][j] = element
+    return multimatrx
 
 def add_bias(X, b):
     """
@@ -21,8 +35,6 @@ def add_bias(X, b):
     return X + b
 
 if __name__ == "__main__":
-    A = np.array([[1, 2], [3, 4]])
-    B = np.array([[5, 6], [7, 8]])
-    a = [1, 2]
-    b = [2, 1]
-    print(dot(a, b))
+    A = np.array([[1, 2, 5, 7], [6, 4, 2, 9], [5, 6, 0, -1]])
+    B = np.array([[2, 5], [4, -3], [0, 1], [-2, 4]])
+    print(matmul(A, B))
